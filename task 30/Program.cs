bf53af7793e83441a4820e423a0ab2ceed1cb283
Kaz-Omarbagaev <Omarbagaef@mail.ru>
2022-07-06 16:09:42 +0300

    добавил задачи

---
+++ task 30/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+void FillArray(int[] any_array)
+{
+    for (int i = 0; i < any_array.Length; i++)
+    {
+        any_array[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] any_array)
+{
+    Console.Write("[");
+    for (int i = 0; i < any_array.Length; i++)
+    {
+        Console.Write($"{any_array[i]}");
+        if (i < any_array.Length - 1)
+        {
+
+            Console.Write(", ");
+
+        }
+    }
+
+    Console.Write("]");
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
---
+++ task 8/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N),
+ *а на выходе показывает все чётные числа от 1 до N.
+ *5 -> 2, 4
+ *8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+//int N = Convert.ToInt32(Console.ReadLine());
+//int N = 0;
+for (int i = 2; i <= Number; i += 2)
+
+    Console.Write("{0}, ", i);
+
+
+
+
+
+
